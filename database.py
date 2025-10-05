f99745bd9207cfe52c0d4c9e5d0c66c109251d0a
Jani-Jain <jjain2603@gmail.com>
2025-10-05 22:10:31 +0530

    original

--- database.py
+++ database.py
@@ -5,4 +5,4 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-print("Something")
+
